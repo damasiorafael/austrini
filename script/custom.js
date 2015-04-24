82faf2ffb31d2661552fb231b50ad7f69ad8773c
damasiorafael <damasio_damasio@hotmail.com>
2015-04-24 06:58:53 -0300

    ambientes quase finalizado

--- script/custom.js
+++ script/custom.js
@@ -92,6 +92,18 @@
 /* ANIMATE EFFECT  ------------------------------------------------------------------- */
 	new WOW().init();
 
+/* COLORBOX        ------------------------------------------------------------------- */
+$(".box-salas").colorbox({
+	rel: "box-salas"
+});
+
+$(".box-cozinhas").colorbox({
+	rel: "box-cozinhas"
+});
+
+$(".box-banheiros").colorbox({
+	rel: "box-banheiros"
+});
 
 /* GRID A LICIOUS  ------------------------------------------------------------------- */
 	$("#article-list", document.body).gridalicious({
